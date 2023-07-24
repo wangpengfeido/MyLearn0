35b283e68487a4475f416473846e7a3c82e3c9c6
wangpengfei <wangpengfei.do@bytedance.com>
2023-07-24 11:31:39 +0800

    sw

--- LearnHtml/49020ServiceWorker/test/public/910test/20110multi-event/sw.js
+++ LearnHtml/49020ServiceWorker/test/public/910test/20110multi-event/sw.js
@@ -1,35 +1,78 @@
 console.log('00000000000');
 
 this.addEventListener('install', function (event) {
-  console.log('------------install');
+  console.log('------------install-1');
+  this.skipWaiting();
+});
+this.addEventListener('install', function (event) {
+  console.log('------------install-2');
   this.skipWaiting();
 });
 
 this.addEventListener('activate', function (event) {
-  console.log('------------activate');
+  console.log('------------activate-1');
+  this.clients.claim();
+});
+this.addEventListener('activate', function (event) {
+  console.log('------------activate-2');
   this.clients.claim();
 });
 
 this.addEventListener('fetch', (event) => {
-  console.log('----------fetch', event);
-  if (event.request.url.includes('send-message-in-sw')) {
-    console.log('-------this', this, this.navigator);
-    this.postMessage({ b: 'bbb' });
-    return;
+  console.log('=======================fetch-1');
+  if (event.request.url.includes('test-fetch')) {
+    console.log('----------test-fetch-1', event);
+
+    event.respondWith(
+      (async () => {
+        const res = await fetch(event.request);
+        console.log('------------test-fetch-1 over');
+        return res;
+      })()
+    );
   }
+  if (event.request.url.includes('my-fetch')) {
+    console.log('----------my-fetch-1', event);
 
-  event.respondWith(
-    (async () => {
-      const res = await fetch(event.request);
-      return res;
-    })()
-  );
+    // event.respondWith(
+    //   (async () => {
+    //     const res = await fetch(event.request);
+    //     console.log('------------my-fetch-1 over');
+    //     return res;
+    //   })()
+    // );
+  }
+});
+this.addEventListener('fetch', (event) => {
+  console.log('=======================fetch-2');
+  if (event.request.url.includes('test-fetch')) {
+    console.log('----------test-fetch-2', event);
+
+    // event.respondWith(
+    //   (async () => {
+    //     const res = await fetch(event.request);
+    //     console.log('------------test-fetch-2 over');
+    //     return res;
+    //   })()
+    // );
+  }
+  if (event.request.url.includes('my-fetch')) {
+    console.log('----------my-fetch-2', event);
+
+    event.respondWith(
+      (async () => {
+        const res = await fetch(event.request);
+        console.log('------------my-fetch-2 over');
+        return res;
+      })()
+    );
+  }
 });
 
 this.addEventListener('message', async (event) => {
-  console.log('--------message1', event, event.data);
+  console.log('--------message-1', event, event.data);
 });
 
 this.addEventListener('message', async (event) => {
-  console.log('--------message2', event, event.data);
+  console.log('--------message-2', event, event.data);
 });
